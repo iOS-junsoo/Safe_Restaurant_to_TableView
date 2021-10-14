b59f6d2c29a4039a456c7fe9a538b7d2d573006c
kimjunsoo <rlawnstn01023@naver.com>
2021-10-14 10:34:42 +0900

    Initial Commit

---
+++ Safe_Restaurant_to_TableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Safe_Restaurant_to_TableView
+//
+//  Created by 준수김 on 2021/10/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
